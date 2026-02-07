f1eb3c7be47560ec463af5199f8ff40c8a59c558
pixieboot <lab_damjanovicn@protonmail.com>
2026-02-07 21:58:07 +0100

    init source dir

---
+++ src/main.cpp
@@ -0,0 +1,149 @@
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
+#include <cmath>
+#include <string_view>
+#include <filesystem>
+#include <thread>
+
+constexpr uint32_t windowStartWidth = 640;
+constexpr uint32_t windowStartHeight = 480;
+
+struct AppContext {
+  SDL_Window* window;
+  SDL_Renderer* renderer;
+  SDL_Texture* messageTex, *imageTex;
+  SDL_FRect messageDest;
+  SDL_AppResult app_quit = SDL_APP_CONTINUE;
+};
+
+SDL_AppResult SDL_Fail() {
+  SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, "Error %s", SDL_GetError());
+  return SDL_APP_FAILURE;
+}
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+  // init the lib, here we make a window so we only need video capabilities
+  if (not SDL_Init(SDL_INIT_VIDEO)) {
+    return SDL_Fail();
+  }
+
+  // init TTF
+  if (not TTF_Init()) {
+    return SDL_Fail();
+  }
+
+  // create a window
+  SDL_Window* window = SDL_CreateWindow("text", windowStartWidth, windowStartHeight, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+  if(not window) {
+    return SDL_Fail();
+  }
+
+  // create a renderer
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+  if(not renderer) {
+    return SDL_Fail();
+  };
+
+  auto basePathPtr = SDL_GetBasePath();
+  if(not basePathPtr) {
+    return SDL_Fail();
+  }
+  const std::filesystem::path basePath = basePathPtr;
+
+  const auto fontPath = basePath / "Inter-VariableFont.ttf";
+  TTF_Font* font = TTF_OpenFont(fontPath.string().c_str(), 36);
+  if(not font) {
+    return SDL_Fail();
+  }
+
+  // render the font to a surface
+  const std::string_view text = "hello nigger";
+  SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, text.data(), text.length(), {255, 255, 255});
+
+  // make a texture from the surface
+  SDL_Texture* messageTex = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+  // font no longer needed, can be destroyed
+  TTF_CloseFont(font);
+  SDL_DestroySurface(surfaceMessage);
+
+  auto messageTexProps = SDL_GetTextureProperties(messageTex);
+  SDL_FRect text_rect {
+    .x = 0,
+    .y = 0,
+    .w = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0)),
+    .h = float(SDL_GetNumberProperty(messageTexProps, SDL_PROP_TEXTURE_WIDTH_NUMBER, 0))
+  };
+
+  // print win info
+  SDL_ShowWindow(window);
+  {
+    int width, height, bbwidth, bbheight;
+    SDL_GetWindowSize(window, &width, &height);
+    SDL_GetWindowSizeInPixels(window, &bbwidth, &bbheight);
+    SDL_Log("Window size: %ix%i", width, height);
+    SDL_Log("Backbuffer size: %ix%i", bbwidth, bbheight);
+    if(width != bbwidth) {
+      SDL_Log("This is a highdpi env");
+    };
+  }
+
+  // setup app data
+  *appstate = new AppContext{
+    .window = window,
+    .renderer = renderer,
+    .messageTex = messageTex,
+    .messageDest = text_rect
+  };
+
+  SDL_SetRenderVSync(renderer, -1); // enable vsync
+
+  SDL_Log("App started successfully");
+
+  return SDL_APP_CONTINUE;
+};
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
+  auto* app = (AppContext*)appstate;
+
+  if(event->type == SDL_EVENT_QUIT) {
+    app->app_quit = SDL_APP_SUCCESS;
+  }
+
+  return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+  auto* app = (AppContext*)appstate;
+
+  // draw a color
+  auto time = SDL_GetTicks() / 1000.f;
+  auto red = (std::sin(time) + 1) / 2.0 * 255;
+  auto green = (std::sin(time / 2) + 1) / 2.0 * 255;
+  auto blue = (std::sin(time) * 2 + 1) / 2.0 * 255;
+
+  SDL_SetRenderDrawColor(app->renderer, red, green, blue, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(app->renderer);
+
+  SDL_RenderPresent(app->renderer);
+
+  return app->app_quit;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+  auto* app = (AppContext*)appstate;
+  if(app) {
+    SDL_DestroyRenderer(app->renderer);
+    SDL_DestroyWindow(app->window);
+    delete app;
+  }
+
+  TTF_Quit();
+  SDL_Log("App exited");
+  SDL_Quit();
+}
